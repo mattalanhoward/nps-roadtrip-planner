cf0bb62b20e11df9103291479bb1e190f6622ee2
Matthew Howard <mattalanhoward@gmail.com>
2020-12-07 14:43:42 +0100

    Db, Cloudinary setup, User/Session models/routes

--- nps-server/app.js
+++ nps-server/app.js
@@ -2,12 +2,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 require("dotenv").config();
+require("./config/db.config");
+
+//Router Definitions
+var indexRouter = require("./routes/index");
+var userRouter = require("./routes/auth");
 
 var app = express();
+
 console.log(process.env.PORT);
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,7 +20,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+//CORS configuration
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.ORIGIN,
+  })
+);
+
+app.use("/user", userRouter);
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 module.exports = app;
